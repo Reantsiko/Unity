018c43385117adb2a7eb61ae4971e8a36da78dcc
Reantsiko <fabien.abbenbroek@gmail.com>
2022-10-31 20:07:50 +0100

    start delivery driver tutorial

---
+++ Delivery Driver/Assets/Driver.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    public float steerSpeed = 1f;
+    public float moveSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveDir = Input.GetAxis("Vertical");
+        var turnDir = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(0, moveSpeed * moveDir) * Time.deltaTime);
+        transform.Rotate(new Vector3(0,0, steerSpeed * -turnDir) * Time.deltaTime);
+    }
+}
