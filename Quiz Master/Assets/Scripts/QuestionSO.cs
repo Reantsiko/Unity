0113f79d0842c1bad0042e8c6fcde303be816943
Reantsiko <fabien.abbenbroek@gmail.com>
2022-11-09 13:26:47 +0100

    added answers to the questions

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
@@ -9,13 +10,15 @@ public class QuestionSO : ScriptableObject
     [TextArea(2,6)]
     private string question = "Enter new question text here";
     [SerializeField]
-    private List<Answer> answers;
+    private List<Answer> answers = new List<Answer>(4);
 
     public string GetQuestion() => question;
+    public int GetCorrectAnswerIndex() => answers.FindIndex(a => a.GetIsCorrect() == true);
     public Answer GetAnswer(int index)
     {
         if (answers == null || index > answers.Count)
             return null;
         return answers[index];
     }
+
 }
